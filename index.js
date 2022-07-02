22494fcb686e8037465f6071fe814b5092138d0f
Jady Chan <jadychan@usc.edu>
2022-07-01 20:24:37 -0700

    final touches

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
 document.getElementById("quiz").onsubmit = function(){ 
     let checks = document.querySelectorAll('input[type="checkbox"]:checked').length;
     let title = "";
-    if (checks > 3) {
-        title = "you are jady chan"
+    if (checks < 5) {
+        title = "damn you are not jady chan"
+    } else if (checks >5 && checks < 11) {
+        title = "i see why we're friends"
+    } else if (checks >10 && checks < 16) {
+        title = "i love ur energy ;)"
+    } else if (checks >14 && checks < 21) {
+        title = "okay BESTIE I LOVE U!!!"
+    } else if (checks >20 && checks < 26) {
+        title = "wowie u can be my partner in crime B)"
+    } else if (checks >25 && checks < 31) {
+        title = "tell my sister we have another sibling.. :0"
+    } else if (checks >31 && checks < 40) {
+        title = "omg u are jady chan... hello :0"
+    } else if (checks == 50) {
+        title = "u can have my passport"
     }
 
     document.getElementById("quiz").innerHTML = `${checks} <br> ${title}`;
